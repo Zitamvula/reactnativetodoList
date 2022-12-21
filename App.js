68ef3893e0f58f2f9a205952a87a596616d477a3
Zita Mvula <Zitamw@gmail.com>
2022-12-21 12:11:55 -0500

    Set up sections and styled them

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+
+      <View style ={styles.container}> 
+      {/* Today's Tasks */}
+      <View styles= {styles.tasksWrapper}>
+        <Text styles= {styles.sectionTitle}> Today's Tasks </Text>
+        <View styles = {styles.items}> 
+         {/* This is where the tasks will go! */}
+
+        </View>
+
+      </View>
+       
+
+      </View>
+
+
       
     </View>
   );
@@ -12,7 +28,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E8EAED',
     
   },
+tasksWrapper: {
+  paddingTop: 80, 
+  paddingHorizontal: 20,
+},
+sectionTitle:{
+  fontSize: 24,
+  fontWeight: 'bold'
+},
+items:{},
 });
